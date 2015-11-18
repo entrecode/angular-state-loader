8d77c8685c10cca47349e6be559833212ac139ad
Felix Roos <roos@entrecode.de>
2015-11-18 17:11:51 +0100

    added stateLoader directive + basic example

---
+++ demo/demo.js
@@ -0,0 +1,32 @@
+/**
+ * Created by felix on 18.11.15.
+ */
+angular.module('stateLoaderDemo', ['ui.router', 'angular-state-loader'])
+
+  .run(['$rootScope', '$state', '$stateParams', function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  }]).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state("basic", {
+      url:         "/",
+      templateUrl: 'basic.tpl.html'
+    }).state('example', {
+      url:        '/example/:delay',
+      resolve:    {
+        data: function($stateParams, $timeout, $q) {
+          var deferred = $q.defer();
+
+          $timeout(function() {
+            deferred.resolve('INCREDIBLE AMOUNT OF DATA');
+          }, $stateParams.delay);
+
+          return deferred.promise;
+        }
+      },
+      template:   '<p>This Page took {{delay}}ms to load!</p>',
+      controller: function($scope, $stateParams, data) {
+        $scope.delay = $stateParams.delay;
+        console.log(data);
+      }
+    });
+  }]);
---
+++ src/angular-state-loader.js
@@ -0,0 +1,78 @@
+/**
+ * Created by felix on 18.11.15.
+ */
+angular.module('angular-state-loader', []).directive('stateLoader', function($rootScope, $timeout) {
+  //credits to http://stackoverflow.com/questions/24200909/apply-loading-spinner-during-ui-router-resolve
+  return {
+    scope:      {
+      fromState: '@', //restrict showing loader only when loading from this state name
+      toState:   '@', //restrict showing loader only when loading this state name
+      delay:     '@', //show loader after ms loading time
+      forceShow: '='
+    },
+    restrict:   'E',
+    transclude: true,
+    replace:    true,
+    compile:    function(elem, attrs, transcludeFn) {
+      var timer;
+      return {
+        pre:  function preLink(scope) {
+          scope.shouldFire = function(fromState, toState) {
+            return (!scope.fromState || fromState.name === scope.fromState) &&
+              (!scope.toState || toState.name === scope.toState) && scope.forceShow === undefined;
+          };
+          scope.hideLoader = function(element, force) {
+            if (!scope.forceShow || force) {
+              if (timer) {
+                $timeout.cancel(timer);
+              }
+              element.addClass('ng-hide');
+            }
+          };
+          scope.showLoader = function(element) {
+            if (scope.delay > 0) {
+              timer = $timeout(function() {
+                element.removeClass('ng-hide');
+              }, scope.delay);
+            } else {
+              element.removeClass('ng-hide');
+            }
+          };
+
+          transcludeFn(elem, function(clone) {
+            //check if element contains custom loading text/html
+            scope.transcluding = clone.text().trim().length > 0;
+          });
+        },
+        post: function postLink(scope, element) {
+          scope.delay = typeof scope.delay === 'number' ? scope.delay : 100;
+
+          scope.$watch('forceShow', function(show) {
+            if (show) {
+              scope.showLoader(element);
+            } else {
+              scope.hideLoader(element);
+            }
+          });
+
+          scope.hideLoader(element, true);
+
+          var show = $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            if (scope.shouldFire(fromState, toState)) {
+              scope.showLoader(element);
+            }
+          });
+          var hide = $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            if (scope.shouldFire(fromState, toState)) {
+              scope.hideLoader(element);
+            }
+          });
+          scope.$on('$destroy', show);
+          scope.$on('$destroy', hide);
+        }
+      };
+    },
+    template:   '<div class="angular-state-loader"><span ng-hide="transcluding">' +
+                '<i class="material-icons rotate-right">autorenew</i>' + '</span><ng-transclude></ng-transclude></div>'
+  };
+});
